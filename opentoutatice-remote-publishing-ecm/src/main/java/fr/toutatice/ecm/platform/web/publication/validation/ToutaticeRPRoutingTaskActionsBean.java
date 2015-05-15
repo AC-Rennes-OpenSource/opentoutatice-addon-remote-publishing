36cf780967ee75487ce742d342ab7afab62967b4
no author <noauthor@ac-rennes.fr>
2015-05-15 14:37:22 +0000

    git-svn-id: http://projet.toutatice.fr/repos/toutatice-ecm/opentoutatice-addons/opentoutatice-remote-publishing/trunk@42461 e030d6d2-7f5c-0410-ab4d-d3efbec72937

--- opentoutatice-remote-publishing-ecm/src/main/java/fr/toutatice/ecm/platform/web/publication/validation/ToutaticeRPRoutingTaskActionsBean.java
+++ opentoutatice-remote-publishing-ecm/src/main/java/fr/toutatice/ecm/platform/web/publication/validation/ToutaticeRPRoutingTaskActionsBean.java
@@ -2,6 +2,9 @@ package fr.toutatice.ecm.platform.web.publication.validation;
 
 import static org.jboss.seam.ScopeType.CONVERSATION;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.jboss.seam.annotations.Install;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
@@ -13,42 +16,49 @@ import org.nuxeo.ecm.webapp.action.MainTabsActions;
 
 import fr.toutatice.ecm.platform.constants.ExtendedSeamPrecedence;
 import fr.toutatice.ecm.platform.constants.ToutaticeRPConstants;
-import fr.toutatice.ecm.platform.core.constants.ToutaticeGlobalConst;
 import fr.toutatice.ecm.platform.core.helper.ToutaticeWorkflowHelper;
 import fr.toutatice.ecm.platform.web.workflows.ToutaticeRoutingTaskActionsBean;
-
 
 @Scope(CONVERSATION)
 @Name("routingTaskActions")
 @Install(precedence = ExtendedSeamPrecedence.ADD_ON)
 public class ToutaticeRPRoutingTaskActionsBean extends ToutaticeRoutingTaskActionsBean {
-		
-	
+
+	private static final long serialVersionUID = 7828257388908697920L;
+	private static final List<String> TOUTATICE_RP_WF_ACTIONS = new ArrayList<String>() {
+		private static final long serialVersionUID = -7017362685680104930L;
+
+		{
+			add(ToutaticeRPConstants.CST_WORKFLOW_BUTTON_VALIDATION_ACCEPT);
+			add(ToutaticeRPConstants.CST_WORKFLOW_BUTTON_VALIDATION_REJECT);
+		}
+	};
+
 	@Override
 	public String endTask(Task task) throws ClientException {
-		DocumentModel currentDoc = navigationContext.getCurrentDocument();
-		Task taskForNotif = new TaskImpl(task.getDocument());
-		String wfInitiator = getWorkFlowInitiator();
-		//#3071 notification
-		String eventName;
-		if(isAcceptOnLineButtonCliked()){
-			eventName = ToutaticeGlobalConst.CST_EVENT_ONLINE_TASK_APPROVED;
-		}else if( ToutaticeRPConstants.CST_WORKFLOW_BUTTON_VALIDATION_ACCEPT.equalsIgnoreCase(super.button)){
-			eventName = ToutaticeRPConstants.CST_EVENT_VALIDATION_TASK_APPROVED;
-		}else if(ToutaticeRPConstants.CST_WORKFLOW_BUTTON_VALIDATION_REJECT.equalsIgnoreCase(super.button)){
-			eventName = ToutaticeRPConstants.CST_EVENT_VALIDATION_TASK_REJECTED;
-		}else{
-			eventName =ToutaticeGlobalConst.CST_EVENT_ONLINE_TASK_REJECTED;
-		}	
-				
-		super.endTask(task);
+		String view = MainTabsActions.DEFAULT_VIEW;
 		
-		ToutaticeWorkflowHelper.notifyRecipients(documentManager, taskForNotif,
-				currentDoc, wfInitiator,
-				eventName);
-
-		
-		return MainTabsActions.DEFAULT_VIEW;
+		if (TOUTATICE_RP_WF_ACTIONS.contains(getClickedButton())) {
+			DocumentModel currentDoc = navigationContext.getCurrentDocument();
+			Task taskForNotif = new TaskImpl(task.getDocument());
+			String wfInitiator = getWorkFlowInitiator();
+			String eventName;
+			
+			if (ToutaticeRPConstants.CST_WORKFLOW_BUTTON_VALIDATION_ACCEPT.equalsIgnoreCase(getClickedButton())) {
+				eventName = ToutaticeRPConstants.CST_EVENT_VALIDATION_TASK_APPROVED;
+			} else {
+				eventName = ToutaticeRPConstants.CST_EVENT_VALIDATION_TASK_REJECTED;
+			}	
+			
+			super.endTask(task);
+			
+			ToutaticeWorkflowHelper.notifyRecipients(documentManager, taskForNotif, currentDoc, wfInitiator, eventName);
+		} else {
+			//no-op. only forward processing to mother class
+			view = super.endTask(task);
+		}
+        
+        return view;
 	}
 
 }
